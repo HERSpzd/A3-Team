e34a6ee077744655f3e13685e52b0fc197ff3969
HERSpzd <1913737070@qq.com>
2024-04-10 23:37:14 +0800

    Update the initial version of homepage
    Integrate the entire project and upload updated homepage

---
+++ Assets/Tang/Scripts/TangStateControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangStateControl : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forward = Input.GetKey("w");
+        bool jump = Input.GetKey("space");
+
+        if (jump) 
+        {
+            animator.SetBool("jump", true);
+        }
+
+        if (!jump) 
+        {
+            animator.SetBool("jump", false);
+        }
+
+
+        if (forward) 
+        {
+            animator.SetBool("walking", true);
+        }
+
+        if (!forward)
+        {
+            animator.SetBool("walking", false);
+        }
+        
+    }
+
+}
